5a8d0dec3cc83f9afd6b857b9eea2f873fc71f5e
longshine <longxianghe@gmail.com>
2012-11-20 11:42:28 +0800

    fix incorrect route in usbstick-winxp-setup

--- platform/mxusbstick/setup/6lowpan-setup-winxp.bat
+++ platform/mxusbstick/setup/6lowpan-setup-winxp.bat
@@ -6,6 +6,6 @@ set /p USBSTICKIF="Please select a interface: "
 
 ipv6 ifc %USBSTICKIF% advertises forwards
 ipv6 adu %USBSTICKIF%/aaaa::1
-ipv6 rtu aaaa::64/ %USBSTICKIF% publish
+ipv6 rtu aaaa::/64 %USBSTICKIF% publish
 
 echo Done!
